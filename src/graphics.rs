5839e04d0cbcbd4a6e4e6a563fcfe8057c53126c
Ask Sødal <asks@uia.no>
2023-02-12 11:44:30 +0100

    flytter til stasjonaer

---
+++ src/graphics.rs
@@ -0,0 +1,35 @@
+use winit::window::window;
+struct State {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: winit::dpi::PhysicalSize<u32>,
+    window: Window,
+}
+
+impl State {
+    async fn new() -> Self {
+        todo!()
+    }
+
+    pub fn window(&self) {
+        &self.window
+    }
+
+    fn resize(&mut self, new_size: winit::dpi::PhyssicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        todo!()
+    }
+}
